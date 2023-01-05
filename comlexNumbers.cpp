ce8fb0381b5ac45581326da50c3e11075e496c2e
Mohammed Ahmed <mohamedstarwars@gmail.com>
2023-01-05 17:05:14 +0200

    Complex Number

---
+++ comlexNumbers.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+struct ComplexNumber
+{
+		int realNum;
+		int imagNum;
+		ComplexNumber(int rN, int iN) : realNum{rN}, imagNum{iN}
+		{};
+
+		ComplexNumber operator+ (const ComplexNumber & oper2)
+		{
+			ComplexNumber result =  ComplexNumber(0,0);
+			result.realNum = oper2.realNum + realNum;
+			result.imagNum = oper2.imagNum + imagNum;
+			return result;
+		}
+
+
+};
+
+int main(void)
+{
+
+	ComplexNumber obj1 = ComplexNumber(2,3);
+	ComplexNumber obj2 = ComplexNumber(3,2);
+
+	ComplexNumber res = obj1 + obj2;
+	std::cout << "Result is " << res.realNum << "+" << res.imagNum <<"i" << std::endl;
+
+	return 0;
+}
